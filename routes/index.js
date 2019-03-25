f21d0a7ff00d6f1b1cab3c5d4ec2fff1ef3afd80
zhangliyuan@sinap.ac.cn <zhangliyuan@sinap.ac.cn>
2019-03-25 19:36:15 +0800

    sf0-list-search

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var pg1 = require('./pgconn');
+var path = require('path');
+var fs = require('fs');
+var pg = require('./pgconn');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+router.get('/sf0List.json', function(req, res, next) {
+  process.env.TZ = "Asia/Shanghai";
+  sql ="select * from sf0_variable order by index ";
+  pg.query(sql, function (result) {
+  res.jsonp(result.rows);
+});
+
+})
