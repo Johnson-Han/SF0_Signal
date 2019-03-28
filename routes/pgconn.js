c24482aacc78e6b8bcd9eadb44c4a4e0ce4af2e4
root <root@db1.prod.dc>
2019-03-29 04:09:55 +0800

    pgconn-change

--- routes/pgconn.js
+++ routes/pgconn.js
@@ -1,10 +1,10 @@
 var pg = require('pg');
 var client = new pg.Client();
 var config = {
-  user: 'zhangliyuan', //env var: PGUSER
-  database: 'tmsr_doc', //env var: PGDATABASE
-  password: 'zhang123', //env var: PGPASSWORD
-  host: '47.100.41.42', // Server hosting the postgres database
+  user: 'postgres', //env var: PGUSER
+  database: 'zhangliyuan', //env var: PGDATABASE
+  password: '123456', //env var: PGPASSWORD
+  host: '127.0.0.1', // Server hosting the postgres database
   port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
